fcb5e09798e931dfcaee5c3c36e1b8f7cee7577d
dmccue <dmccue@localhost>
2014-01-26 07:07:55 -0800

    got all the quotes in for the jeep stage

--- unityProject/Assets/Scripts/Jeep.cs
+++ unityProject/Assets/Scripts/Jeep.cs
@@ -23,6 +23,7 @@ public class Jeep : MonoBehaviour
 		timePassed = 0;
 	}
 
+	GameObject isTalking;
 	void Update()
 	{
 		timePassed += Time.deltaTime;
@@ -42,9 +43,28 @@ public class Jeep : MonoBehaviour
 			GamepadState state = GamePad.GetState(GamePad.Index.One);
 			float baseSpeed = 30.0f;
 			rBody.velocity = new Vector2((state.LeftStickAxis.x + 2) * baseSpeed, rBody.velocity.y);
+			if (state.X && !isTalking)
+			{
+				isTalking = TheManager.Say(gameObject, drivingJokes[UnityEngine.Random.Range(0, drivingJokes.Length) % drivingJokes.Length], 3.0f);
+			}
 			break;
 		}
 		//transform.eulerAngles = new Vector3(0, 0, transform.eulerAngles.z * 0.9f + rBody.velocity.y/10 * 0.1f);
 		transform.eulerAngles = new Vector3(0, 0, rBody.velocity.y/10);
 	}
+
+	static string[] drivingJokes = new string[]{
+		"If the opposite\nof Pro is Con.\nIs the opposite of\nProgress, Congress?",
+		"Why do they\nsterilise needles,\nused for\nlethal injections?",
+		"Why isn't\nginger red?",
+		"If corporations are\npeople, is the\nstock market\nslave trading?",
+		"If life is\nunfair to everyone,\nis it fair?",
+		"Is this\nstick shift?",
+		"Hey! Did you\nremember to fill\nup the gas tank?",
+		"*putt* *putt* poof.\nI think our car\nis running on fumes",
+		"Sorry that was me,\nI had a burrito",
+		"Are we there yet?",
+		"And I'd like to\ntake a minute\njust sit right\nthere...",
+		"I just wanna tell\nyou how I'm feeling,\nGotta make you\nunderstand..."
+	};
 }
--- unityProject/Assets/Scripts/TheManager.cs
+++ unityProject/Assets/Scripts/TheManager.cs
@@ -32,7 +32,7 @@ public class TheManager : MonoBehaviour
 		//player.GetComponent<Rigidbody2D>().isKinematic = true;
 	}
 
-	GameObject Say(GameObject gObj, string text, float talkStayTime)
+	public static GameObject Say(GameObject gObj, string text, float talkStayTime)
 	{
 		// give a word bubble for the jeep
 		var talkBubble = new GameObject();
@@ -42,13 +42,13 @@ public class TheManager : MonoBehaviour
 		spriteRenderer.sprite = talkSprite;
 		talkBubble.name = "talkBubble";
 		Destroy(talkBubble, talkStayTime);
-		talkBubble.transform.parent = jeep.transform;
+		talkBubble.transform.parent = gObj.transform;
 		talkBubble.transform.position = new Vector2 (gObj.transform.position.x, gObj.transform.position.y + 20);
 		talkBubble.transform.localScale = new Vector2(1.1f, 1.1f);
 		
 		var talkText = new GameObject ();
 		talkText.transform.parent = gObj.transform;
-		talkText.transform.position = new Vector3 (jeep.transform.position.x, jeep.transform.position.y +13, -5.0f);
+		talkText.transform.position = new Vector3 (gObj.transform.position.x, gObj.transform.position.y +13, -5.0f);
 		Destroy(talkText, talkStayTime);
 		//talkText.renderer.material.color = Color.black;
 		//talkText.transform.localScale = new Vector2(10, 10);
@@ -106,81 +106,11 @@ public class TheManager : MonoBehaviour
 		{
 			//
 			talkBubble = Say(jeep, "Come with me\nIf you want\nto live!", 3.0f);
-#if IGNORE
-			float talkStayTime = 3.0f;
-			
-			// give a word bubble for the jeep
-			talkBubble = new GameObject();
-			Sprite talkSprite = Resources.Load<Sprite>("Art/Effects/speechbubble");
-			
-			SpriteRenderer spriteRenderer = talkBubble.AddComponent<SpriteRenderer> ();
-			spriteRenderer.sprite = talkSprite;
-			talkBubble.name = "talkBubble";
-			Destroy(talkBubble, talkStayTime);
-			talkBubble.transform.parent = jeep.transform;
-			talkBubble.transform.position = new Vector2 (jeep.transform.position.x, jeep.transform.position.y + 20);
-			talkBubble.transform.localScale = new Vector2(1.1f, 1.1f);
-			
-			talkText = new GameObject ();
-			talkText.transform.parent = jeep.transform;
-			talkText.transform.position = new Vector3 (jeep.transform.position.x, jeep.transform.position.y +13, -5.0f);
-			Destroy(talkText, talkStayTime);
-			//talkText.renderer.material.color = Color.black;
-			//talkText.transform.localScale = new Vector2(10, 10);
-			
-			MeshRenderer meshRenderer = talkText.AddComponent<MeshRenderer>();
-			TextMesh talkLayer = talkText.AddComponent<TextMesh> ();
-			//meshRenderer.material = (Resources.Load("Livingst") as Material);
-			
-			talkText.renderer.material = Resources.Load("arialbd", typeof(Material)) as Material; 
-			talkLayer.text = "Hop in!";
-			talkLayer.anchor = TextAnchor.LowerCenter;
-			talkLayer.fontSize = 16; //20;
-			talkLayer.characterSize = 2.0f;
-			talkLayer.renderer.material.color = Color.black;
-			Font myFont = Resources.Load("arialbd", typeof(Font)) as Font;
-			talkLayer.font = myFont;
-#endif
 		}
 		else if (timePassed > 62 && !talkBubble) // 65
 		{
 			//
 			talkBubble = Say(jeep, "Don't look now,\nwe're being chased\nby Godzilla!", 3.0f);
-#if IGNORE
-			float talkStayTime = 3.0f;
-
-			// give a word bubble for the jeep
-			talkBubble = new GameObject();
-			Sprite talkSprite = Resources.Load<Sprite>("Art/Effects/speechbubble");
-			
-			SpriteRenderer spriteRenderer = talkBubble.AddComponent<SpriteRenderer> ();
-			spriteRenderer.sprite = talkSprite;
-			talkBubble.name = "talkBubble";
-			Destroy(talkBubble, talkStayTime);
-			talkBubble.transform.parent = jeep.transform;
-			talkBubble.transform.position = new Vector2 (jeep.transform.position.x, jeep.transform.position.y + 20);
-			talkBubble.transform.localScale = new Vector2(1.1f, 1.1f);
-			
-			talkText = new GameObject ();
-			talkText.transform.parent = jeep.transform;
-			talkText.transform.position = new Vector3 (jeep.transform.position.x, jeep.transform.position.y +13, -5.0f);
-			Destroy(talkText, talkStayTime);
-			//talkText.renderer.material.color = Color.black;
-			//talkText.transform.localScale = new Vector2(10, 10);
-			
-			MeshRenderer meshRenderer = talkText.AddComponent<MeshRenderer>();
-			TextMesh talkLayer = talkText.AddComponent<TextMesh> ();
-			//meshRenderer.material = (Resources.Load("Livingst") as Material);
-			
-			talkText.renderer.material = Resources.Load("arialbd", typeof(Material)) as Material; 
-			talkLayer.text = "Don't look now,\nwe're being chased\nby Godzilla!";
-			talkLayer.anchor = TextAnchor.LowerCenter;
-			talkLayer.fontSize = 16; //20;
-			talkLayer.characterSize = 2.0f;
-			talkLayer.renderer.material.color = Color.black;
-			Font myFont = Resources.Load("arialbd", typeof(Font)) as Font;
-			talkLayer.font = myFont;
-#endif
 		}
 		else if (timePassed > 61)
 		{
