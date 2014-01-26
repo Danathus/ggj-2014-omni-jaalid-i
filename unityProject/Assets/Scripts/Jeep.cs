08f82d30422f8c1d9e30f07b356b067d54d52b83
dmccue <dmccue@localhost>
2014-01-26 03:08:13 -0800

    now the Jeep will follow at a distance until 40 seconds has passed, when it will catch up quick and follow right behind the party on the left edge of the screen

--- unityProject/Assets/Scripts/Jeep.cs
+++ unityProject/Assets/Scripts/Jeep.cs
@@ -6,15 +6,24 @@ using CBX.TileMapping.Unity;
 
 public class Jeep : MonoBehaviour
 {
+	public Camera camera;
+	float timePassed;
 	void Start()
 	{
 		transform.position += new Vector3(0, 20, 0);
+		timePassed = 0;
 	}
 
 	void Update()
 	{
+		timePassed += Time.deltaTime;
+
+		float measuredDistanceFromCamera = (camera.transform.position - transform.position).magnitude;
+		float desiredDistanceFromCamera = timePassed > 40.0f ? 10.0f : 100.0f;
+		float speed = measuredDistanceFromCamera - desiredDistanceFromCamera; // 40
+
 		Rigidbody2D rBody = GetComponent<Rigidbody2D>();
-		rBody.velocity = new Vector2(40, rBody.velocity.y);
+		rBody.velocity = new Vector2(speed, rBody.velocity.y);
 		//transform.eulerAngles = new Vector3(0, 0, transform.eulerAngles.z * 0.9f + rBody.velocity.y/10 * 0.1f);
 		transform.eulerAngles = new Vector3(0, 0, rBody.velocity.y/10);
 	}
